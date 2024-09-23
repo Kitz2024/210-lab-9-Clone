817a7a709b2052e6376f3202d358d7faf1f62a01
Kitz2024 <kit.pollinger@gmail.com>
2024-09-22 20:32:32 -0700

    Clone commit

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
-        cout << i << " ";
-
+    for (int i = 1; i <= 100; i++){
+        cout << i << endl;
+    }
     return 0;
 }
